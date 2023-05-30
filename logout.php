b46cee0608c3a36c36a88e8374d99d5eb5e0f691
WAW1311 <115858209+WAW1311@users.noreply.github.com>
2023-05-30 20:19:51 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -6,28 +6,4 @@ session_abort();
 
 header('location: login.php')
 
-?><?php
-session_start();
-
-session_destroy();
-session_abort();
-
-header('location: login.php')
-
-?><?php
-session_start();
-
-session_destroy();
-session_abort();
-
-header('location: login.php')
-
-?><?php
-session_start();
-
-session_destroy();
-session_abort();
-
-header('location: login.php')
-
 ?>
